99dbd2e7794b8e4793ff9b78a36180c183958a4e
qqw  wee <a@a.ru>
2018-12-28 08:28:34 +0300

    html fix

--- authentication/admin.py
+++ authentication/admin.py
@@ -5,7 +5,9 @@ from authentication.models import *
 
 @admin.register(SteamUser)
 class SteamUserAdmin(admin.ModelAdmin):
-    list_filter = ('vip', 'outlaw','is_squad_leader')
+    list_filter = ('vip', 'outlaw', 'is_squad_leader')
+    search_fields = ('steamid', 'discord_id', 'discord_nickname', 'personaname',)
+    ordering = ('personaname',)
 
 
 admin.site.register(PrivateMessages)
