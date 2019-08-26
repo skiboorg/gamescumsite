5e5d69502784fdeb041cbb8b1a8ed0d1ec746526
ddnnss <ddnnss.i1@gmail.com>
2019-08-26 21:07:49 +0300

    add logging game chat in discord

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,6 @@
 from django.contrib.auth import logout
 from django.shortcuts import render
-from django.http import HttpResponseRedirect,HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-
+from django.http import HttpResponseRedirect, HttpResponse
 from news.models import News
 from authentication.forms import EditProfileForm
 from squads.forms import *
@@ -11,22 +9,21 @@ from squads.models import *
 from shop.models import Orders
 import json
 from django.contrib import messages
-#from datetime import datetime , time
+# from datetime import datetime , time
 from lxml import html
 import requests
-import bot_settings
 from django.http import JsonResponse
 from pages.models import *
 from datetime import datetime, timedelta
 import bot_settings
 from discord_webhook import DiscordWebhook, DiscordEmbed
+from django.views.decorators.csrf import csrf_exempt
 
 
 @csrf_exempt
 def kill_log(request):
     logTime = request.POST.get('time')
     logText = json.loads(request.POST.get('data'))
-
 
     logtext, created = KillLog.objects.get_or_create(LogTime=logTime, defaults={'oldLog': logText})
 
@@ -53,12 +50,20 @@ def kill_log(request):
         victimGameX = ''
         victimGameY = ''
         isEventKill = False
+
+        # const
+        # factor = mapId.height() / 1200000;
+        # this.zoom.kx = ((this.feed.kx * -1) + 613142.340) * factor;
+        # this.zoom.ky = (this.feed.ky + 593500) * factor;
+        # this.zoom.vx = ((this.feed.vx * -1) + 613142.340) * factor;
+        # this.zoom.vy = (this.feed.vy + 593500) * factor;
+
         if DF:
             for msg in DF:
-                msg = msg.replace(': ',',').replace('S[',',').replace('C[',',').replace('] (killer',',').split(',')
-                print('kill log=' , msg)
-                victimNick = msg[2].replace(' ','')[:msg[2].find('(')-1]
-                victimID = msg[2].replace(' ','')[msg[2].find('('):msg[2].find(')')-1]
+                msg = msg.replace(': ', ',').replace('S[', ',').replace('C[', ',').replace('] (killer', ',').split(',')
+                print('kill log=', msg)
+                victimNick = msg[2].replace(' ', '')[:msg[2].find('(') - 1]
+                victimID = msg[2].replace(' ', '')[msg[2].find('('):msg[2].find(')') - 1]
                 killerNick = msg[4].replace(' ', '')[:msg[4].find('(') - 1]
                 victimID = msg[4].replace(' ', '')[msg[4].find('('):msg[4].find(')') - 1]
                 killerGameX = msg[6]
@@ -66,39 +71,24 @@ def kill_log(request):
                 victimGameX = msg[10]
                 victimGameY = msg[11]
 
-
-
-
                 try:
                     if msg[22]:
                         isEventKill = True
                 except:
                     pass
-
-
     return HttpResponse(status=200)
+
 
 @csrf_exempt
 def chat_log(request):
-
-    steam_url = 'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=BD1EBFF1F1644726E7F1399B2E9FF8B4&steamids=76561198031013162'
-
-    # test=["2019.08.26-07.10.37: '76561198005560814:Non(2495)' 'Local: уже идем'", "2019.08.26-07.10.44: '76561198294173616:CTAPOBEP(34)' 'Global: за тобой)'", "2019.08.26-07.10.46: '76561198836595365:Alpha(2736)' 'Global: сервак пвп да?'", "2019.08.26-07.10.49: '76561198005560814:Non(2495)' 'Local: )'", "2019.08.26-07.10.52: '76561198119304883:Djony Falcony(2046)' 'Global: Так точно!'"]
-    # for msg in test:
-    #     msg = msg.replace(": '", ':').replace("' '",':').replace("'",'')
-    #     print (msg)
     logTime = request.POST.get('time')
     logText = json.loads(request.POST.get('data'))
     print(logTime)
 
-
-    logtext, created = GameChat.objects.get_or_create(logTime=logTime,defaults={'oldLog':logText})
+    logtext, created = GameChat.objects.get_or_create(logTime=logTime, defaults={'oldLog': logText})
 
     if created:
         print('new log is created')
-
-
-
     else:
         print('log is present')
 
@@ -112,31 +102,6 @@ def chat_log(request):
         logtext.oldLog = newlog
         logtext.save(force_update=True)
 
-        # server_response = requests.get(
-        #     'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=BD1EBFF1F1644726E7F1399B2E9FF8B4&steamids={}&format=json'.format(
-        #        '76561198031013162'))
-        # json_data = json.loads(server_response.text)
-        # print('get user avatar')
-
-        # userAvatar = json_data['response']['players'][0]['avatar']
-
-
-        # webhook_url = 'https://discordapp.com/api/webhooks/614005912662835215/Un9HD3x5x6XZWGYhgdHzLrwfM4EF9uYA8RfLTniCssIUnsvzFZE8wKaUxaNsmWJvBTQR'
-        # webhook = DiscordWebhook(url=webhook_url)
-        # embed = DiscordEmbed(title="Сообщение из чата")
-        # embed.set_author(name="автор", icon_url='{}'.format(userAvatar))
-        #
-        # embed.set_footer(text="12.23.2333 12:32:23")
-        # webhook.add_embed(embed)
-        # webhook.execute()
-
-        # const
-        # factor = mapId.height() / 1200000;
-        # this.zoom.kx = ((this.feed.kx * -1) + 613142.340) * factor;
-        # this.zoom.ky = (this.feed.ky + 593500) * factor;
-        # this.zoom.vx = ((this.feed.vx * -1) + 613142.340) * factor;
-        # this.zoom.vy = (this.feed.vy + 593500) * factor;
-
         if DF:
             for msg in DF:
                 msg = msg.replace(": '", ':').replace("' '", ':').replace("'", '')
@@ -149,43 +114,20 @@ def chat_log(request):
                 msg_time[0] = str(int(msg_time[0]) + 3)
 
                 server_response = requests.get(
-                    'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=BD1EBFF1F1644726E7F1399B2E9FF8B4&steamids={}&format=json'.format(
-                        msg[1]))
+                    'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={}&steamids={}&format=json'.format(
+                        bot_settings.STEAM_KEY, msg[1]))
                 json_data = json.loads(server_response.text)
                 print('get user avatar')
-
                 userAvatar = json_data['response']['players'][0]['avatar']
 
-                webhook = DiscordWebhook(url=webhook_url)
-                # embed = DiscordEmbed(title='-'.join(msg_date) + ' - ' + ':'.join(msg_time))
-                # embed.add_embed_field(name=msg[2][:msg[2].find('(')], value=msg[4])
-                # webhook.add_embed(embed)
-                # webhook.execute()
-
+                webhook = DiscordWebhook(url=bot_settings.DISCORD_CHAT_WH)
                 embed = DiscordEmbed(title=msg[4], color=0xec4e00)
                 embed.set_author(name=msg[2][:msg[2].find('(')], icon_url='{}'.format(userAvatar))
 
                 embed.set_footer(text='-'.join(msg_date) + ' - ' + ':'.join(msg_time))
                 webhook.add_embed(embed)
                 webhook.execute()
-
-    #if request.GET:
-
-        #print(json.loads(request.GET.get('data')))
-
-
-        # newlog = json.loads(request.GET.get('data'))
-        # Dict1 = newlog.readlines()
-        # Dict2 = oldlog.readlines()
-        # DF = [x for x in Dict1 if x not in Dict2]
-        # print(DF)
-
-
-
-
-
     return HttpResponse(status=200)
-
 
 
 def server_stat(request):
@@ -200,16 +142,20 @@ def server_stat(request):
         total_deaths += player.deaths
     return render(request, 'pages/statistic.html', locals())
 
+
 def vip(request):
     return render(request, 'pages/vip.html', locals())
 
+
 def discord(request):
     return HttpResponseRedirect('https://discord.gg/sgUz53k')
+
 
 def settings(request):
     page_title = 'НАСТРОЙКИ'
     site_settings = SiteStat.objects.get(id=1)
     return render(request, 'pages/settings.html', locals())
+
 
 def get_squad_info(player_id):
     try:
@@ -219,7 +165,7 @@ def get_squad_info(player_id):
         squad_info = False
     if squad_member:
         squad_info = Squad.objects.get(id=squad_member.squad.id)
-    return(squad_info)
+    return (squad_info)
 
 
 def support(request):
@@ -227,7 +173,6 @@ def support(request):
 
 
 def index(request):
-
     page_title = 'ГЛАВНАЯ'
     index_page_active = 'active'
     news = News.objects.all().order_by('-id')[:12]
@@ -267,7 +212,9 @@ def profile(request, nickname_req):
 
     def get_play_time(steamid):
         played_time = 0
-        server_response = requests.get('http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=BD1EBFF1F1644726E7F1399B2E9FF8B4&steamid={}&format=json'.format(steamid))
+        server_response = requests.get(
+            'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={}&steamid={}&format=json'.format(
+                bot_settings.STEAM_KEY, steamid))
         json_data = json.loads(server_response.text)
         print('get user info')
         print(json_data['response'])
@@ -282,7 +229,9 @@ def profile(request, nickname_req):
 
     def get_bans(steamid):
 
-        server_response = requests.get('http://api.steampowered.com/ISteamUser/GetPlayerBans/v1/?key=BD1EBFF1F1644726E7F1399B2E9FF8B4&steamids={}'.format(steamid))
+        server_response = requests.get(
+            'http://api.steampowered.com/ISteamUser/GetPlayerBans/v1/?key={}&steamids={}'.format(bot_settings.STEAM_KEY,
+                                                                                                 steamid))
         json_data = json.loads(server_response.text)
         print('get user bans')
 
@@ -300,7 +249,6 @@ def profile(request, nickname_req):
             player_bans = 'НЕТ ДАННЫХ'
         return player_bans
 
-
     if request.POST:
 
         form = EditProfileForm(request.POST, instance=request.user)
@@ -314,9 +262,7 @@ def profile(request, nickname_req):
         if request.user.is_authenticated:
             if nickname_req == request.user.nickname:
 
-
                 own_profile = True
-                player_logs = PlayerLog.objects.filter(player_id=request.user.id).order_by('-created')
 
                 player = request.user
                 squad_info = get_squad_info(player.id)
@@ -342,7 +288,6 @@ def profile(request, nickname_req):
 
                 form = EditProfileForm(instance=request.user)
                 section = request.GET.get('section')
-
 
                 return render(request, 'pages/ownprofile_new.html', locals())
             else:
@@ -395,6 +340,7 @@ def about_us(request):
     page_title = 'О ПРОЕКТЕ'
     return render(request, 'pages/about_us_new.html', locals())
 
+
 def reset_limit(request):
     player = request.user
     player.buys_count = 0
@@ -405,15 +351,11 @@ def reset_limit(request):
                                   player_action='Сброс лимита покупок. До сброса баланс : {}.  После сброса {} '.format(
                                       old_wallet, player.wallet))
     new_log.save()
-    PlayerLog.objects.create(player_id=player.id,
-                             log_action='Списание RC',
-                             comment='С баланса списано 1000 RC за сброс лимита на покупки.'
-                                     'Прежний баланс {}, новый баланс {}'.format(old_wallet,
-                                                                                 player.wallet)).save()
 
     messages.add_message(request, messages.SUCCESS, 'Дневной лимит сброшен, приятных покупок;) ')
 
     return HttpResponseRedirect('/profile/' + request.user.nickname)
+
 
 def bonus_pack(request):
     admins = SteamUser.objects.filter(is_staff=True)
@@ -429,7 +371,7 @@ def bonus_pack(request):
     player.bonus_pack = True
     player.save(force_update=True)
     messages.add_message(request, messages.SUCCESS, 'Заявка на получение бонус-пака отправлена! '
-                                                 'Ожидай в дискорде сообщения от администрации сервера.')
+                                                    'Ожидай в дискорде сообщения от администрации сервера.')
 
     webhook = DiscordWebhook(url=bot_settings.SHOP_ADMIN_URL)
     embed = DiscordEmbed(title='ЗАЯВКА НА БОНУС ПАК', color=0xec4e00)
@@ -439,9 +381,6 @@ def bonus_pack(request):
     webhook.add_embed(embed)
 
     webhook.execute()
-    PlayerLog.objects.create(player_id=player.id,
-                             log_action='Запрос',
-                             comment='Запрос на бонус-пак успешно отправлен').save()
 
     return HttpResponseRedirect('/profile/' + request.user.nickname)
 
@@ -449,6 +388,7 @@ def bonus_pack(request):
 def rules(request):
     page_title = 'ПРАВИЛА СЕРВЕРА'
     return render(request, 'pages/rules_new.html', locals())
+
 
 def players(request):
     all_players = SteamUser.objects.filter(is_staff=False, is_active=True)
@@ -459,6 +399,7 @@ def players(request):
 
     page_title = 'СТАТИСТИКА ИГРОКОВ'
     return render(request, 'pages/top20_new.html', locals())
+
 
 def buy_vip(request):
     player = request.user
@@ -473,11 +414,6 @@ def buy_vip(request):
                                                     ' Баланс кошелька после покупки : {}. '.format(old_player_wallet,
                                                                                                    player.wallet))
         new_log.save()
-        PlayerLog.objects.create(player_id=player.id,
-                                 log_action='Списание RC',
-                                 comment='С баланса списано 20000 RC за покупку VIP.'
-                                         'Прежний баланс {}, новый баланс {}'.format(old_player_wallet,
-                                                                                     player.wallet)).save()
     else:
         new_log = Logs.objects.create(player_id=request.user.id,
                                       player_action='Попытка покупки VIP за RC. Баланс кошелька до покупки : {}.'.format
@@ -490,19 +426,18 @@ def about_bonus_pack(request):
     page_title = 'БОНУС ПАК НОВЫМ ИГРОКАМ'
     return render(request, 'pages/bonus_pack.html', locals())
 
+
 def add_to_player_balance(request):
     player = request.user
     to_player = SteamUser.objects.get(id=request.POST.get('player_id'))
     if player.wallet >= int(request.POST.get('rc_amount')):
-        old_player_wallet = player.wallet
-        old_to_player_wallet = to_player.wallet
         player.wallet -= int(request.POST.get('rc_amount'))
         player.save(force_update=True)
         to_player.wallet += int(request.POST.get('rc_amount'))
         to_player.save(force_update=True)
         new_log = Logs.objects.create(player_id=request.user.id,
                                       player_action='Перевод {} RC, игроку {} '.format(
-                                        request.POST.get('rc_amount'), to_player.nickname))
+                                          request.POST.get('rc_amount'), to_player.nickname))
         new_log.save()
         new_message = PrivateMessages.objects.create(to_player_id=to_player.id,
                                                      from_player_name=player.personaname,
@@ -512,22 +447,8 @@ def add_to_player_balance(request):
                                                      .format(request.POST.get('rc_amount')))
         new_message.save()
         messages.add_message(request, messages.SUCCESS,
-                             'Баланс игрока '+ to_player.personaname +' успешно пополнен на '
+                             'Баланс игрока ' + to_player.personaname + ' успешно пополнен на '
                              + request.POST.get('rc_amount') + ' RC')
-        PlayerLog.objects.create(player_id=player.id,
-                                 log_action='Списание RC',
-                                 comment='С баланса списано {} RC за перевод игроку {}.'
-                                         'Прежний баланс {}, новый баланс {}'.format(request.POST.get('rc_amount'),
-                                                                                     to_player.personaname,
-                                                                                     old_player_wallet,
-                                                                                     player.wallet)).save()
-        PlayerLog.objects.create(player_id=to_player.id,
-                                 log_action='Пополнение RC',
-                                 comment='На баланс зачислено {} RC от игрока {}'
-                                         'Прежний баланс {}, новый баланс {}'.format(request.POST.get('rc_amount'),
-                                                                                     player.personaname,
-                                                                                     old_to_player_wallet,
-                                                                                     to_player.wallet)).save()
     else:
         new_log = Logs.objects.create(player_id=request.user.id,
                                       player_action='Попытка перевода {} RC, игроку {} '.format(
